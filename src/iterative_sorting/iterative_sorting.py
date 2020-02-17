58fd39bfc90a4e5603d936145bdaab0e48559c28
chudnofskyATD <mpereannor@gmail.com>
2020-02-17 17:51:01 +0000

    bubble_sort v1

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -21,11 +21,16 @@ def selection_sort( arr ):
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
-  
-  
+def bubble_sort(arr):
+  sorted = False 
+  while not sorted: 
+    sorted = True
+    for i in range(0, len(arr) -1):
+      if arr[i] > arr[i + 1]:
+        sorted = False
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]  
 
-    return arr
+  return arr
 
 
 # STRETCH: implement the Count Sort function below
